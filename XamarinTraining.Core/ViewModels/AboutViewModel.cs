5ad0673f04d73941485f41bbde33340c333aab74
nicruo <nicruo@gmail.com>
2021-09-22 19:13:24 +0100

    first commit

---
+++ XamarinTraining.Core/ViewModels/AboutViewModel.cs
@@ -0,0 +1,40 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Threading.Tasks;
+using XamarinTraining.Core.Domain;
+using XamarinTraining.Core.Services;
+
+namespace XamarinTraining.Core.ViewModels
+{
+    public class AboutViewModel : ViewModelBase
+    {
+        private IDataService dataService;
+        private IToastService toastService;
+        private int counter;
+
+
+        private string aboutTitle;
+        public string AboutTitle
+        {
+            get => aboutTitle;
+            set => Set(nameof(AboutTitle), ref aboutTitle, value);
+        }
+
+        //public HomeViewModel(IDataService dataService, IToastService toastService)
+        //{
+        //    this.dataService = dataService;
+        //    this.toastService = toastService;
+        //}
+
+        //private async void LoadApplicationTitleAsync()
+        //{
+        //    toastService.ShowToast("button clicked");
+        //    await Task.Delay(1000);
+        //    ApplicationTitle = counter++ + " volta(s)";
+        //    Character result = await dataService.GetCharacterAsync(2);
+        //    ApplicationTitle = "Result = " + result;
+        //}
+
+        
+    }
+}
---
+++ XamarinTraining.Droid/Activities/AboutActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using GalaSoft.MvvmLight.Helpers;
+using GalaSoft.MvvmLight.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XamarinTraining.Core.ViewModels;
+
+namespace XamarinTraining.Droid.Activities
+{
+    [Activity]
+    class AboutActivity : AppCompatActivity
+    {
+        private EditText editText;
+        private TextView textView;
+        private AboutViewModel viewModel;
+        private Binding binding;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_about);
+            editText = FindViewById<EditText>(Resource.Id.editText);
+            textView = FindViewById<TextView>(Resource.Id.textView);
+
+            viewModel = SimpleIoc.Default.GetInstance<AboutViewModel>();
+            binding = this.SetBinding(() => editText.Text, () => textView.Text, BindingMode.TwoWay);
+            viewModel.AboutTitle = "About title";
+        }
+    }
+}
